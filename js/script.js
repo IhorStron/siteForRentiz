94292788dd064789ed0f5cb6b18e5f15d7ee7900
IhorStron <worklife0025@gmail.com>
2023-10-19 18:23:18 +0300

    added section gallery

--- js/script.js
+++ js/script.js
@@ -62,6 +62,22 @@ const popularSlider = new Swiper('.popular-slider', {
 });
 
 
+//---Gallery
+
+const galleryItems = document.querySelectorAll(".gallery__item");
+
+if (galleryItems.length > 0) {
+	galleryItems.forEach(item => {
+		new Swiper(item, {
+			slidesPerView: 1, 
+			autoplay: {
+				delay: 5000,
+			},
+			effect: 'fade',
+		})
+	})
+}
+
  
 
 
